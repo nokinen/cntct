c877ae0a649fe11c3b9fedf731f5788a28e05ce9
Tobias Noiges <tobias@noig.es>
2012-10-29 23:00:55 +0100

    Add basic info to root

--- endpoint.rb
+++ endpoint.rb
@@ -1,5 +1,9 @@
 require 'sinatra'
 
+get '/' do
+  "This is the endpoint for a form webhook from unbounce that can be reached via: <pre>POST /submit</pre>"
+end
+
 post '/submit' do
   logger.info "Params: #{params}"
 end
