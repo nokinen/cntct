ae5447916d466edaf894fc8874d43a07f7979c5d
Tobias Noiges <tobias@noig.es>
2012-10-30 10:05:23 +0100

    Add logic for request parsing and mail dispatch

--- endpoint.rb
+++ endpoint.rb
@@ -1,9 +1,30 @@
 require 'sinatra'
+require 'json'
+require 'pony'
 
 get '/' do
-  "This is the endpoint for a form webhook from unbounce that can be reached via: <pre>POST /submit</pre>"
+  "The endpoint for the form webhook from unbounce can be reached via: <pre>POST /submit</pre>"
 end
 
 post '/submit' do
-  logger.info "Params: #{params}"
+  
+  Pony.mail to: "tobias@noig.es", from: "noreply@webhook.heroku.com", body: "Sending mail works!"
+  
+  # logger.info "Params: #{params}"
+  #   
+  #   if json = params["data.json"] then
+  #     data = JSON.parse json
+  #     name = data["name"] && data["name"].first
+  #     email = data["email_adress"] && data["email_adress"].first
+  #     phone = data["phone_number_or_skype_contact"] && data["phone_number_or_skype_contact"].first
+  #     time = data["best_timt_to_call"] && data["best_timt_to_call"].first
+  #   end
+  #   
+  #   if name and email and phone and time then
+  #     Pony.mail to: 'tobias@noig.es', from: email, subject: 'Phone Interview Request', 
+  #       body: "Please call me (#{phone}) in the #{time}"
+  #   else
+  #     return 400
+  #   end
+  
 end
