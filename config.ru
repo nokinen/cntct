20831a3bf1590c869c7dd6a17b3b342527887b40
Tobias Noiges <tobias@noig.es>
2012-10-30 10:47:17 +0100

    Add sendgrid config for heroku

--- config.ru
+++ config.ru
@@ -1,2 +1,18 @@
 require './endpoint'
+
+configure :production do 
+  Pony.options = {
+    via: :smtp,
+    via_options: {
+      address: 'smtp.sendgrid.net',
+      port: '587',
+      domain: 'heroku.com',
+      user_name: ENV['SENDGRID_USERNAME'],
+      password: ENV['SENDGRID_PASSWORD'],
+      authentication: :plain,
+      enable_starttls_auto: true
+    }
+  }
+end
+
 run Sinatra::Application
--- endpoint.rb
+++ endpoint.rb
@@ -7,24 +7,18 @@ get '/' do
 end
 
 post '/submit' do
-  
-  Pony.mail to: "tobias@noig.es", from: "noreply@webhook.heroku.com", body: "Sending mail works!"
-  
-  # logger.info "Params: #{params}"
-  #   
-  #   if json = params["data.json"] then
-  #     data = JSON.parse json
-  #     name = data["name"] && data["name"].first
-  #     email = data["email_adress"] && data["email_adress"].first
-  #     phone = data["phone_number_or_skype_contact"] && data["phone_number_or_skype_contact"].first
-  #     time = data["best_timt_to_call"] && data["best_timt_to_call"].first
-  #   end
-  #   
-  #   if name and email and phone and time then
-  #     Pony.mail to: 'tobias@noig.es', from: email, subject: 'Phone Interview Request', 
-  #       body: "Please call me (#{phone}) in the #{time}"
-  #   else
-  #     return 400
-  #   end
-  
+    if params["data.json"] then
+      data = JSON.parse params["data.json"]
+      name = data["name"] && data["name"].first
+      email = data["email_adress"] && data["email_adress"].first
+      phone = data["phone_number_or_skype_contact"] && data["phone_number_or_skype_contact"].first
+      time = data["best_timt_to_call"] && data["best_timt_to_call"].first
+    end
+    
+    if name and email and phone and time then
+      Pony.mail to: 'tobias@noig.es', from: email, subject: 'Phone Interview Request', 
+        body: "Please call me (#{phone}) in the #{time}."
+    else
+      return 400
+    end
 end
